df34a510b6c94e0c4ad93bc958775f7f795bf529
SamuelPerez27 <samuelpperez07@gmail.com>
2021-04-23 13:04:36 -0300

    Update Login y Comienzo de aplicacion

--- traftrol/src/Login.js
+++ traftrol/src/Login.js
@@ -18,6 +18,7 @@ const Login = (props) => {
    return (
    <section  className="login"> 
         <div className="loginContainer">
+            <img src="traftrol\src\Img\Logo.png" className="ImagenLogin" alt="Logo de la Pagina"/> 
            <label>Usuario</label>
            <input type = "text" autoFocus required value={email} onChange= {e => setEmail(e.target.value)}/>
            <p  className="errorMsg">{emailError}</p>
@@ -28,13 +29,13 @@ const Login = (props) => {
           <div className="btnContainer">
                {hasAccount ? (
                    <>
-                     <button onClick= {handelLogin}   >Ingresar</button>
-                     <p>¿ No tienes una Cuenta ?  <span onClick ={() => sethasAccount(!hasAccount)} >Registrate</span> </p>
+                     <button onClick= {handelLogin}>Ingresar</button>
+                     <p>¿No tienes una Cuenta ?  <span onClick ={() => sethasAccount(!hasAccount)} >Registrate</span> </p>
 
                    </>
                ): (
                 <>
-                <button  onClic = {handleSignup}>Registrate</button>
+                <button  onClick = {handleSignup}>Registrate</button>
                 <p> ¿Aún no Tienes Una Cuenta?  <span  onClick ={() => sethasAccount(!hasAccount)} >Ingresar</span> </p>
 
               </>
