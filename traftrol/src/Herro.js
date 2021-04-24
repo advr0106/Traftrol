73a5c7157d4c1c38671f7b2747dd8e2e8f0fb280
Alex Valenzuela <alexvalenzuela0108@gmail.com>
2021-04-24 08:12:31 -0400

    .

--- traftrol/src/Herro.js
+++ traftrol/src/Herro.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect, Component } from 'react';
 import logo from '../src/Img/Logo.png'
+import FireBase from '../src/FireBase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 const circles = document.getElementsByClassName('circle')
 let activeLight = 0;
+let estado= '';
+let tiempo='apagado';
+
+const firestore = FireBase.firestore();
+let semaRef = firestore.collection('semaforo').doc('green');
 
 function changeLight() {
   circles[activeLight].className = 'circle';
@@ -17,19 +27,46 @@ function changeLight() {
   currentLight.classList.add(currentLight.getAttribute('color'));
 }
 
-setInterval(() => {
-  changeLight();
-}, 1000);
-
 const Hero = (props) => {
-  const [seconds, setSeconds] = React.useState(50);
-  React.useEffect(() => {
-    if (seconds > 0) {
-      setTimeout(() => setSeconds(seconds - 1), 1000);
-    } else {
-      setSeconds(50);
+  const [seconds, setSeconds] = React.useState(0);
+  const semaforoRef = firestore.collection('semaforo');
+  const query = semaforoRef;
+  const [semaforos] = useCollectionData(query, { idField: 'id' });
+  useEffect(() => {
+    if(tiempo=='encendido'){
+      if (seconds > 0) {
+        setTimeout(() => setSeconds(seconds - 1), 1000);
+      } else {
+        estado='Adelante, ya puedes cruzar';
+        setSeconds(0);
+        circles[activeLight].className = 'circle';
+      }
+    }else{
+      setSeconds(0);
     }
   });
+
+  function activado(){
+    estado ='Ten paciencia, ya llegará tu turno :)';
+    tiempo = 'encendido';
+    circles[activeLight].className = 'circle';
+    activeLight= 2;
+    const currentLight = circles[activeLight]
+  
+    currentLight.classList.add(currentLight.getAttribute('color'));
+    
+    semaRef.get().then((doc) =>{
+      setSeconds(Number(doc.data().tiempoactual))
+    })
+    
+  }
+  
+  function cancelar(){
+    circles[activeLight].className = 'circle';
+    estado ='Ya no cruzarás :(';
+    tiempo = 'apagado';
+    setSeconds(0);
+  }
 
   const {
     email, setEmail,
@@ -43,12 +80,12 @@ const Hero = (props) => {
       <nav>
         <div className="contain">
 
-        <h2>
-          <img src={logo} width="60" />
+          <h2>
+            <img src={logo} width="60" />
             Bienvenido a  <span className="Trafword" >
-            <b> Traftol </b>
-          </span>
-        </h2>
+              <b> Traftol </b>
+            </span>
+          </h2>
         </div>
         <button onClick={handleLogout} >Cerrar Sesión</button>
       </nav>
@@ -69,7 +106,7 @@ const Hero = (props) => {
 
               <h2 className="Semaforotexto">Semáforo Actualmente </h2>
               <div className="container">
-                <div className="circle red" color="red"></div>
+                <div className="circle" color="red"></div>
                 <div className="circle" color="yellow"></div>
                 <div className="circle" color="green"></div>
               </div>
@@ -81,6 +118,9 @@ const Hero = (props) => {
                 <div className="count">{seconds}</div>
               </div>
             </div>
+            <input type="button" value="Presiona para avisar que cruzarás" onClick={activado}/>
+            <input type="button" value="Presiona para cancelar cruzarás" onClick={cancelar}/>
+            <p>{estado}</p>
           </div>
         </section>
       </section>
